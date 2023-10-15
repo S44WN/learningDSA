e755287c5d621832623b5fa35f19d82ea140bb7a
Sawan <itissaavn@gmail.com>
2023-10-16 00:02:10 +0530

    add other mwthods

--- Array ADT/array.cpp
+++ Array ADT/array.cpp
@@ -168,6 +168,120 @@ int RecursiveBS(int a[], int low, int high, int key)
     }
 }
 
+int Get(struct Array arr, int index)
+{
+    if (index >= 0 && index < arr.length)
+        return arr.A[index];
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int x)
+{
+    if (index >= 0 && index < arr->length)
+        arr->A[index] = x;
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    int i;
+    for (i = 1; i < arr.length; i++)
+    {
+        if (arr.A[i] > max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    int i;
+    for (i = 1; i < arr.length; i++)
+    {
+        if (arr.A[i] < min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int Sum(struct Array arr)
+{
+    int s = 0;
+    int i;
+    for (i = 0; i < arr.length; i++)
+        s += arr.A[i];
+    return s;
+}
+
+float Avg(struct Array arr)
+{
+    return (float)Sum(arr) / arr.length;
+}
+
+// with auxiliary array
+void Reverse(struct Array *arr)
+{
+    int *B;
+    int i, j;
+
+    B = new int[arr->length]; // creating auxiliary array
+
+    // revesing array
+    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
+    {
+        B[j] = arr->A[i];
+    }
+
+    // copying reversed array to original array
+    for (i = 0; i < arr->length; i++)
+    {
+        arr->A[i] = B[i];
+    }
+};
+
+// without auxiliary array - with swapping
+
+void ReverseWSwapping(struct Array *arr)
+{
+    int i, j;
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
+    {
+        swap(&arr->A[i], &arr->A[j]);
+    }
+};
+
+// inserting in an sorted array
+
+void InsertSort(struct Array *arr, int x)
+{
+    int i = arr->length - 1;
+    if (arr->length == arr->size)
+    {
+        return;
+    }
+    while (i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr)
+{
+    int i;
+    for (i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
